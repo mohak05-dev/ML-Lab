8b2e9e5f713ab84375879f4b6a791129a6c9ce5b
mohak05-dev <mohakmaji9@gmail.com>
2026-09-21 21:32:07 +0530

    Created 1.1_numpy_statistics.py

---
+++ Assignment-1/1.1_numpy_statistics.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+marks = np.array([78, 85, 92, 67, 88, 75, 95, 81, 73, 89])
+
+print("Mean:", np.mean(marks))
+print("Median:", np.median(marks))
+print("Standard Deviation:", np.std(marks))
+print("Maximum:", np.max(marks))
+print("Minimum:", np.min(marks))
